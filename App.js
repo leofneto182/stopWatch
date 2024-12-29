d2967a70b052d0856da50cb9153ca6793f97fcd3
Leonidio Neto <leonidio.freitas@outlook.com>
2024-12-29 17:42:20 +0000

    Updating file

--- App.js
+++ App.js
@@ -1,118 +1,185 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React, { useState } from "react";
+import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+let timer = null;
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+let mss = 0;
+let ss = 0;
+let mm = 0;
+let hh = 0;
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+function App(){
+
+  const [number, setNumber] = useState('00:00:00');
+  const [goButton, setGoButton]= useState('Start');
+  const [lastLap, setLastLap] = useState(null);
+
+  function start(){
+    
+    if(timer !== null){
+      // It will stop the timer
+    clearInterval(timer);
+    timer = null;
+
+    setGoButton('Start');
+    }else{
+      //It starts the timer
+      timer = setInterval(() => {
+        mss++;
+
+        if(mss == 60){
+          mss = 0;
+          ss++;
+        }
+
+        if(ss == 60){
+          ss = 0,
+          mm++;
+        }
+
+        if(mm == 60){
+          mm = 0;
+          hh++;
+        }
+
+        let formatWithHour = 
+        (hh < 10 ? '0' + hh : hh) + ':'
+        + (mm < 10 ? '0' + mm : mm) + ':'
+        + (ss < 10 ? '0' + ss : ss) + ':'
+        + (mss < 10 ? '0' + mss : mss);
+
+        let formatWithoutHour =
+        (mm < 10 ? '0' + mm : mm) + ':'
+        + (ss < 10 ? '0' + ss : ss) + ':'
+        + (mss < 10 ? '0' + mss : mss);
+
+
+
+        if(hh < 1){
+          setNumber(formatWithoutHour);
+        }else{
+          setNumber(formatWithHour)
+        }
+
+      }, 1);
+
+      setGoButton('Lap');
+
+    }
+
+
+  }
+
+  function clear(){
+
+    if(timer !== null){
+      // It stops the timer
+      clearInterval(timer);
+      timer = null;
+    }
+
+    setLastLap(number);
+
+    setNumber('00:00:00');
+    mss = 0;
+    ss = 0;
+    mm = 0;
+    hh = 0; 
+
+    setGoButton('Start');
+
+
+
+  }
+
+  return(
+
+    <View style={styles.container}>
+      <View style={styles.imgView}>
+      <Image source={require('./src/stopWatch.png')} style={styles.img}/>
+
+      <Text style={styles.timer}>{number}</Text>
+      </View>
+
+      <View style={styles.btnArea}>
+      <TouchableOpacity style={styles.btn} onPress={ start }>
+          <Text style={styles.btnText}>
+            {goButton}
+          </Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.btn} onPress={ clear }>
+          <Text style={styles.btnText}>
+            Clear
+          </Text>
+      </TouchableOpacity>
+      </View>
+
+      <Text style={styles.lapText}>{lastLap ? 'Last Lap: ' + lastLap : ''}</Text>
+
     </View>
+
   );
 }
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+let styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#00aeef',
+  },
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  img:{
+    width: 300,
+    height: 370,
+  },  
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
+  timer:{
+    fontSize: 45,
+    color: '#FFF',
+    marginTop: -175,
+  },
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  imgView:{
+    alignItems: 'center',
+    justifyContent: 'center',
+
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+
+  btn:{
+    flex: 1,
+    backgroundColor: '#FFF',
+    borderWidth: 1,
+    borderColor: '#0086ef',
+    margin: 17,
+    borderRadius: 10,
+    height: 45,
+    justifyContent: 'center',
+    alignItems: 'center'
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+
+  btnArea:{
+    flexDirection: 'row',
+    height: 40,
+    marginTop: 150,
   },
-  highlight: {
-    fontWeight: '700',
+
+  btnText:{
+    fontSize: 22,
+    fontWeight: 'bold',
+    color: '#00aeef'
   },
+
+  lapText:{
+    marginTop: 70,
+    fontSize: 25,
+    color: '#FFF',
+    fontStyle: 'italic'
+  }
+
+
 });
 
 export default App;
